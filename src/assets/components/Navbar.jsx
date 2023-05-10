7b94acfd31f0e14d9c71135c2065db22b55779b5
Lloyd Carie Mendoza Obligado <lobligado@intercommerce.com.ph>
2023-05-10 23:13:09 +0800

    enhancement: Animation and Icons

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -47,10 +47,10 @@ const Navbar = () => {
     <div className="w-full h-[68px] md:h-[112px] lg:pl-[164px] lg:pr-[164px] md:pl-[50px] md:pr-[50px] pl-[24px] pr-[24px] flex items-center bg-white ">
       <div className="w-full h-[68px] md:h-[112px] flex items-center justify-between md:border-b-2 md:border-[#F7F8FD]">
         <div className="h-[112px] flex items-center">
-          <img className="md:hidden pr-4 cursor-pointer" onClick={handleMenu} src="src/assets/images/icon-menu.svg" alt="menu" />
-          <img className="w-[137px] mr-[42px] cursor-pointer" src="src/assets/images/logo.svg" alt="sneaker logo" />
+          <img className="md:hidden pr-4 cursor-pointer" onClick={handleMenu} src="./src/assets/images/icon-menu.svg" alt="menu" />
+          <img className="w-[137px] mr-[42px] cursor-pointer" src="./src/assets/images/logo.svg" alt="sneaker logo" />
           <ul className={`md:relative md:w-auto md:h-[112px] md:flex ${slideMenu ? 'hidden' : 'flex'} md:flex-row md:items-center fixed h-full bg-white top-0 left-0 flex-col w-[75%]`}>
-            <img className='md:hidden block mt-[25px] ml-[25px] mb-[36px] w-[15px] h-[15px] cursor-pointer' onClick={handleMenu} src="src/assets/images/icon-close.svg" alt="close" />
+            <img className='md:hidden block mt-[25px] ml-[25px] mb-[36px] w-[15px] h-[15px] cursor-pointer' onClick={handleMenu} src="./src/assets/images/icon-close.svg" alt="close" />
             {NavList}
           </ul>
         </div>
@@ -60,7 +60,7 @@ const Navbar = () => {
             <img
               className="cursor-pointer"
               onClick={handleCart}
-              src="src/assets/images/icon-cart.svg"
+              src="./src/assets/images/icon-cart.svg"
               alt="cart"
             />
 
@@ -80,7 +80,7 @@ const Navbar = () => {
 
                         <div className='mt-[28px] flex flex-col justify-center items-center'>
                             <div className='w-[312px] flex justify-between items-center hover:bg-slate-50 transition duration-300'>
-                                <img className='w-[50px] h-[50px] rounded-[4px]' src="src/assets/images/image-product-1-thumbnail.jpg" alt="item"/>
+                                <img className='w-[50px] h-[50px] rounded-[4px]' src="./src/assets/images/image-product-1-thumbnail.jpg" alt="item"/>
                                 <div>
                                     <p className='font-medium text-[16px] leading-[19.84px] text-bla
                                     '>Fall Limited Edition Sneakers</p>
@@ -89,11 +89,11 @@ const Navbar = () => {
                                         
                                     </div>
                                 </div>
-                                <img className='w-[14px] h-[16px] cursor-pointer' src="src/assets/images/icon-delete.svg" alt="delete icon" />
+                                <img className='w-[14px] h-[16px] cursor-pointer' src="./src/assets/images/icon-delete.svg" alt="delete icon" />
                             </div>
 
                             <button className='flex justify-center items-center font-bold  mt-[26px] w-[312px] h-[54px] text-base text-white bg-[#FF7D1A] hover:bg-[#d56815] rounded-[10px] hover:shadow-lg hover:shadow-[#fbd8bf] transition duration-300'>
-                                <img className='2xl:mr-4 mr-[12.8px]' src="src/assets/images/icon-cart-light.svg" alt="cart icon"  /> 
+                                <img className='2xl:mr-4 mr-[12.8px]' src="./src/assets/images/icon-cart-light.svg" alt="cart icon"  /> 
                                 Add to Cart
                             </button>
                         </div>
@@ -104,7 +104,7 @@ const Navbar = () => {
           </div>
           <img
             className="md:w-[50px] md:h-[50px] w-[24px] h-[24px] hover:border-[4px] hover:rounded-full hover:border-[#FF7D1A] cursor-pointer transition duration-300"
-            src="src/assets/images/image-avatar.png"
+            src="./src/assets/images/image-avatar.png"
             alt="avatar"
           />
         </div>
