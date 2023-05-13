4ce0cc81c5aac6a2754dbf36719e4bad343386d8
Lloyd Carie Mendoza Obligado <lobligado@intercommerce.com.ph>
2023-05-13 19:10:07 +0800

    replace assets

--- src/utils/product-photos.js
+++ src/utils/product-photos.js
@@ -1,22 +1,22 @@
 const ProductPhotos = [
     {
         id: 1,
-        image: "../assets/images/image-product-1-thumbnail.jpg",
+        image: "assets/images/image-product-1-thumbnail.jpg",
         name: "image-product-1-thumbnail"
     },
     {
         id: 2,
-        image: "../assets/images/image-product-2-thumbnail.jpg",
+        image: "assets/images/image-product-2-thumbnail.jpg",
         name: "image-product-2-thumbnail"
     },
     {
         id: 3,
-        image: "../assets/images/image-product-3-thumbnail.jpg",
+        image: "assets/images/image-product-3-thumbnail.jpg",
         name: "image-product-3-thumbnail"
     },
     {
         id: 4,
-        image: "../assets/images/image-product-4-thumbnail.jpg",
+        image: "assets/images/image-product-4-thumbnail.jpg",
         name: "image-product-4-thumbnail"
     }
 ]
